59485b604bc2d4b42a906deaaa01ec75743c4298
🥷⚔️💀👻 <contact@stelo.dev>
2024-05-09 16:36:52 -0700

    fix: typo in env variable

--- bin/app.ts
+++ bin/app.ts
@@ -3,5 +3,5 @@ import { App } from 'aws-cdk-lib';
 import { SteloWebPipelineStack } from '../src/stack/pipeline';
 
 const app = new App();
-new SteloWebPipelineStack(app, { env: { account: process.env.STELO_STIE_ACCOUNT, region: 'us-east-1' } });
+new SteloWebPipelineStack(app, { env: { account: process.env.STELO_SITE_ACCOUNT, region: 'us-east-1' } });
 app.synth();
